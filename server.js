69e1163c22e50ad6d001ead85c770cb2211c7185
Shubham Jadhav <shub13711jadhav@gmail.com>
2025-06-08 22:59:17 +0530

    Update server.js
    updated secret key

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const SECRET_KEY = 'your_secret_key';
+const SECRET_KEY = '1ed421a0da50ccaa8678ef339eef5b759fcb5da2b155c82d120789c7c84a4e73eac8fdfae11fe4e4dfcec5b64d211ba5674c963deca4bc0aa518ed1f0b58dc959528eff0e92be99afb09ac1140f31b7266335d32ce0e1595f6cfa5136f092b31124d9ef03739e86c75d58a83d125491101d9acb203037f6c765c15975c9ee1ac2dda82a807b21fd77b6b0905a0f8e1bb87f8a9f84662f18e291a6792a1374e0a939c8e404eb178f4b1d293e8cfea38cc6ba81e71ede994561ef7d35d1d65d20dd8fb384f40010f527731cdca8b23e0afe7592ca71a4758d5c7cbfe5cca57801ce37b659e02736096b519ba9c8b6b3b93d4a5d20868f8e75aed52429b80f3856f';
 
 // Login route
 app.post('/login', (req, res) => {
